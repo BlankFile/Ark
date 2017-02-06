a8c55526a540b82e5cc161f9c13a1638ea580295
BlankFile <arc2t.rk@gmail.com>
2017-02-06 23:44:53 +0900

    test

--- Sample/Converters/CheckTwoColorsConverter.cs
+++ Sample/Converters/CheckTwoColorsConverter.cs
@@ -21,6 +21,7 @@ namespace Sample.Converters
             return (color1.Color == color2.Color);
         }
 
+
         public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             throw new Exception();
